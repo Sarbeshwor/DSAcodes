50ec9f5717d5ef236283fff3d65082a9984a43ec
Sarbeshwor Ghimire <98800172+Sarbeshwor@users.noreply.github.com>
2024-09-19 16:56:49 +0600

    Update StackLinkedList.h

--- Offline2/Stack - Assn/StackLinkedList.h
+++ Offline2/Stack - Assn/StackLinkedList.h
@@ -37,36 +37,47 @@ public:
     // Push an element onto the stack
     void push(int x)
     {
-       //write your code here. Create a new node with the value x and push it onto the stack
-       //update the topNode and currentSize
+        // write your code here. Create a new node with the value x and push it onto the stack
+        Node *newNode = new Node(x, topNode);
+        //    newNode->next->data=x;
+        // update the topNode and currentSize
+        //    topNode->data=x;
+        topNode = newNode;
+        currentSize++;
     }
 
     // Remove and return the top element
     int pop()
     {
-        //write your code here. Check if the stack is empty and return -1 if it is.
-        //pop the top element and return it
-        //update the topNode and currentSize
-        //delete the node that was popped
+        // write your code here. Check if the stack is empty and return -1 if it is.
+        if (isEmpty())
+        {
+            return -1;
+        }
+
+        // pop the top element and return it
+
+        // update the topNode and currentSize
+        // delete the node that was popped
     }
 
     // Return the top element without removing it
     int top()
     {
-        //write your code here. Check if the stack is empty and return -1 if it is.
-        //return the top element
+        // write your code here. Check if the stack is empty and return -1 if it is.
+        // return the top element
     }
 
     // Return the number of elements in the stack
     int length()
     {
-        //write your code here. Return the number of elements in the stack
+        // write your code here. Return the number of elements in the stack
     }
 
     // Check if the stack is empty
     bool isEmpty()
     {
-        //write your code here. Return true if the stack is empty, false otherwise
+        // write your code here. Return true if the stack is empty, false otherwise
     }
 
     // Clear the stack
